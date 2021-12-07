f1ad64bf3ed1962793e92aa16ba08753b9123203
Cotorra <theafterburner750@gmail.com>
2021-12-07 17:28:09 -0300

    Updated request

--- data.py
+++ data.py
@@ -12,12 +12,12 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
-def tarot():
+def data():
     try:
         language = request.args.get("language")
         call_type = request.args.get("type")
     except:
-        return "Bad Request", 400
+        return "Invalid arguments", 200
 
     file_src = f'data/{language}/{call_type}.json'
     with open(file_src, "r", encoding='utf-8') as pack:
