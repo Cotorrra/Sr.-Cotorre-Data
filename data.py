2b614314495cfa590f7b888cc7c393c5dab50b01
Cotorra <30855649+Cotorrra@users.noreply.github.com>
2022-07-16 08:38:23 +0000

    Updated data

--- data.py
+++ data.py
@@ -29,16 +29,16 @@ def data():
     try:
         language = request.args.get("language")
         call_type = request.args.get("type")
+
+        file_src = f'data/{language}/{call_type}.json'
+        with open(file_src, "r", encoding='utf-8') as pack:
+            response = json.load(pack)
+    
+        response = json.dumps(response)
+
+        return Response(response, mimetype='application/json')
+
     except:
         return "Invalid arguments", 200
-
-    file_src = f'data/{language}/{call_type}.json'
-    with open(file_src, "r", encoding='utf-8') as pack:
-        response = json.load(pack)
-
-    response = json.dumps(response)
-
-    return Response(response, mimetype='application/json')
-
 
 serve(app, host="0.0.0.0", port=8080)
